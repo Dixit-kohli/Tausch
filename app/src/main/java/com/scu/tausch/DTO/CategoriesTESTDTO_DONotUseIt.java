2aa4ce8f08d5abe245aef6385f67baf2af90ddb3
Sonali <sonalishilimkar@gmail.com>
2016-01-24 12:12:39 -0800

    Test file- please do not use it..

---
+++ app/src/main/java/com/scu/tausch/DTO/CategoriesTESTDTO_DONotUseIt.java
@@ -0,0 +1,27 @@
+package com.scu.tausch.DTO;
+
+/**
+ * Created by Pratyusha on 1/22/2016.
+ */
+public class CategoriesTESTDTO_DONotUseIt {
+
+    private String categoryId;
+    private String categoryName;
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+}
